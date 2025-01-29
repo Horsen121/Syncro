6a8ececdff39b025ba9d503af34a97f692dc90ab
Horsen121 <vova.mitychin@mail.ru>
2025-01-29 18:20:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/syncro/application/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.syncro.application
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.syncro.application.ui.theme.SyncroTheme
+import com.example.syncro.presentation.ui.screens.LoginScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SyncroTheme {
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
